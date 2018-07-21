88efdbc8ac371b7f6d6b5ff5b6ff5181623887af
honestgraphics <jeannie@honestgraphics.com>
2018-07-20 22:27:07 -0400

    created componentDidMount/axios get request for table

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 // import PropTypes from 'prop-types'
 
 import './Table.scss'
 import EditBtn from '../EditBtn/editbtn';
 
-const Table = () => (
-  <div className="container">
-    <table className="table table-sm table-hover">
+export default class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tracks: []
+    }
+  }
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/auditions')
+    .then((res) => {
+      const { data } = res;
+      this.setState(prevState => ({
+        ...prevState,
+        tracks: data
+      }));
+    })
+  }
+ render() {
+   const { tracks } = this.state;
+   return (<div className="container-fluid">
+   <div className="row">
+   <div className="col-md-12">
+            <div className="panel-body">
+    <table className="table table-sm table-hover table-responsive">
       <thead className="thead-dark">
         <tr>
           <th className="responsive-visibility" scope="col">
@@ -60,29 +82,62 @@ const Table = () => (
         </tr>
       </thead>
       <tbody>
-        <tr>
-          {/* <th scope="row">1</th> */}
-          <td>autofill</td>
-          <td>autofill</td>
-          <td>autofill</td>
-          <td>autofill</td>
-          <td>checkbox</td>
-          <td>checkbox</td>
-          <td>link</td>
-          <td>autofill</td>
-          <td>dropdown box</td>
-          <td>form box</td>
-          <td>form box</td>
-          <td>form box</td>
-          <td>form box</td>
-          <td>form box</td>
-          <td>form box</td>
-          <td><EditBtn /> & Delete Link</td>
-
-        </tr>
+        {tracks.map((track) => (<tr>
+          <td>
+            autofill
+          </td>
+          <td>
+            autofill
+          </td>
+          <td>
+            autofill
+          </td>
+          <td>
+            autofill
+          </td>
+          <td>
+            checkbox
+          </td>
+          <td>
+            checkbox
+          </td>
+          <td>
+           {track.auditionSongLink}
+          </td>
+          <td>
+            autofill
+          </td>
+          <td>
+            dropdown box
+          </td>
+          <td>
+            form box
+          </td>
+          <td>
+            form box
+          </td>
+          <td>
+            form box
+          </td>
+          <td>
+            form box
+          </td>
+          <td>
+            form box
+          </td>
+          <td>
+            form box
+          </td>
+          <td>
+            <EditBtn />
+            & Delete Link
+          </td>
+        </tr>))}
       </tbody>
     </table>
-  </div>
-)
-
-export default Table
+    </div>
+    </div>
+    </div>
+  </div>)
+ }
+}
