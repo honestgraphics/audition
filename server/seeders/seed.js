e15ca734255d437dd759231a32151c80444a3b58
Waleed Hamza <hamzah.waleed@gmail.com>
2018-07-24 17:58:43 -0400

    cleaned out warrnings

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/audition_users' 
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/audition' 
 const connection = mongoose.createConnection(connectionString)
 const models = require('../models')(connection)
 
