2773a10d0c6f04f3bbae3ec187928585f31ddb10
thill2 <hill341539@gmail.com>
2018-07-26 09:36:49 -0400

    commits to edit btn
    Working on getting data from form to appear in MongoDB.  Currently 2 items are with this code.  Previous code threw errors immediately when any value was put into the form.

--- client/src/components/EditBtn/editbtn.js
+++ client/src/components/EditBtn/editbtn.js
@@ -5,11 +5,12 @@ import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import Modal from '../Modal/modal';
 import API from "../../api/api";
+import axios from "axios";
 
 class EditBtn extends Component {
   constructor(props) {
     super(props);
-
+        
     this.state = {
       isOpen: false,
       track: {
@@ -24,6 +25,10 @@ class EditBtn extends Component {
         songArtist: ''
       }
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.updateTrack = this.updateTrack.bind(this);
+
   }
 
   toggleModal = () => {
@@ -33,12 +38,44 @@ class EditBtn extends Component {
     console.log('got here');
   }
 
+  handleChange = event => {
+    // console.log(this.state)
+    // alert('sup')
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+      
+    });
+    // console.log(value)
+  };
+
   updateTrack = (event) => {
     event.preventDefault();
-    this.setState(prevState => ({
-      ...prevState,
-      [event.target.id]: event.target.value
-    }));
+    console.log("inside update track")
+    let id = this.state.id;
+    let songTitle = this.state.songTitle;
+    // let auditionLink = this.state.auditionLink;
+    let albumTitle = this.state.albumTitle;
+    let category = this.state.category;
+    let isrc = this.state.isrc;
+    let filePath = this.state.filePath;
+    let recordLabel = this.state.recordLabel;
+    let songArtist = this.state.songArtist;
+    let formData = {id, songTitle, albumTitle, category, isrc, filePath, recordLabel, songArtist }
+    console.log(typeof formData)
+    console.log(formData)
+
+
+    axios.post("/api/auditions/", formData).then(res => {
+      console.log(res, "unique")});
+
+
+    // this.setState(prevState => ({
+    //   ...prevState,
+    //   [event.target.id]: event.target.value
+    // })
+  // );
   }
 
   // for this function to do something 
@@ -55,12 +92,12 @@ class EditBtn extends Component {
     // })
   }
 
-  updateAuditionLink = (link) => {
-    this.setState(prevState => ({
-      ...prevState,
-      auditionLink: link
-    }));
-  }
+  // updateAuditionLink = (link) => {
+  //   this.setState(prevState => ({
+  //     ...prevState,
+  //     auditionLink: link
+  //   }));
+  // }
 
   openTrack = (auditionLink) => {
     window.open(auditionLink, '', 'width=320, height=75')
@@ -70,7 +107,7 @@ class EditBtn extends Component {
     const {
       id,
       songTitle,
-      auditionLink,
+      // auditionLink,
       albumTitle,
       category,
       isrc,
@@ -105,7 +142,8 @@ class EditBtn extends Component {
                         <label htmlFor="auditionLink">Audition Link</label>
                       </div>
                       <div className="input-group mb-3">
-                        <input type="text" className="form-control" value={auditionLink} disabled>{auditionLink}</input>
+                        {/* <input type="text" className="form-control" value={auditionLink} placeholder={auditionLink}>{auditionLink}</input> */}
+                        {/* disabled ^wasa above*/}
                         <div className="input-group-append">
                           <button onClick={() => this.openTrack('https://auditionbucket.s3.amazonaws.com/tracks/4.wav')} className="btn btn-success">
                             <i className="fas fa-play"></i>
@@ -118,12 +156,12 @@ class EditBtn extends Component {
                       <div className="col">
                         {/* ID Input */}
                         <label htmlFor="id">ID</label>
-                        <input value={id} className="form-control" onChange={this.updateTrack} />
+                        <input value={this.state.id} name="id" className="form-control" onChange={this.handleChange} />
                       </div>
                       <div className="col">
                         {/*Song Title Input */}
                         <label htmlFor="songTitle">Song Title</label>
-                        <input onChange={this.updateTrack} type="text" className="form-control" id="songTitle" placeholder="My Song" value={songTitle} />
+                        <input value={this.state.songTitle} name="songTitle"onChange={this.handleChange} type="text" className="form-control" id="songTitle" placeholder="My Song" />
                       </div>
                     </div>
                     {/* Modal Row 3 */}
@@ -131,12 +169,12 @@ class EditBtn extends Component {
                       <div className="col">
                         {/* Category Input */}
                         <label htmlFor="category">Category</label>
-                        <input className="form-control" id="category" value={category} onChange={this.updateTrack} />
+                        <input value={this.state.category} name="category" className="form-control" id="category"  onChange={this.handleChange} />
                       </div>
                       <div className="col">
                         {/* ISRC Input */}
                         <label htmlFor="isrc">ISRC</label>
-                        <input className="form-control" id="isrc" value={isrc} onChange={this.updateTrack} />
+                        <input value={this.state.isrc} name="isrc" className="form-control" id="isrc"  onChange={this.handleChange} />
                       </div>
                     </div>
                     {/* Modal Row 4 */}
@@ -144,12 +182,12 @@ class EditBtn extends Component {
                       <div className="col">
                         {/* Record Label Input */}
                         <label htmlFor="recordLabel">Record label</label>
-                        <input className="form-control" id="recordLabel" value={recordLabel} onChange={this.updateTrack} />
+                        <input value={this.state.recordLabel} name="recordLabel" className="form-control" id="recordLabel"  onChange={this.handleChange} />
                       </div>
                       <div className="col">
                         {/* Artist Name Input */}
                         <label htmlFor="songArtist">Artist Name</label>
-                        <input className="form-control" id="songArtist" value={songArtist} onChange={this.updateTrack} />
+                        <input value={this.state.songArtist} name="songArtist"className="form-control" id="songArtist" onChange={this.handleChange} />
                         </div>
                     </div>
                     {/* Modal Row 5 */}
@@ -157,20 +195,21 @@ class EditBtn extends Component {
                       <div className="col">
                         {/* Album Name Input */}
                         <label htmlFor="albumTitle">Album Name</label>
-                        <input className="form-control" id="albumTitle" value={albumTitle} onChange={this.updateTrack} />
+                        <input value={this.state.albumTitle} name="albumTitle"className="form-control" id="albumTitle" value={albumTitle} onChange={this.handleChange} />
                       </div>
                       <div className="col">
                         {/* File Path Input */}
                         <label htmlFor="filePath">File Path</label>
-                        <input className="form-control" id="filePath" value={filePath} onChange={this.updateTrack} />
+                        <input value={this.state.filePath} name="filePath" className="form-control" id="filePath" value={filePath} onChange={this.handleChange} />
                         </div>
                     </div>
                   </form>
                 </div>
                 <div className="modal-footer">
                   <button type="button" className="btn btn-secondary" onClick={this.toggleModal} data-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={() => { this.handleSave() }}
-                    disabled={!auditionLink}>Save changes</button>
+                    <button type="submit" className="btn btn-primary" data-dismiss="modal" onClick= {this.updateTrack} >Save changes</button>
+                  {/* disabled={!auditionLink} was in button above */}
+                
                 </div>
             </div>
         </div>
