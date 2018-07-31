26359d2e4b197f2e0c8c6f2b8753de3195418b3f
honestgraphics <jeannie@honestgraphics.com>
2018-07-30 22:52:20 -0400

    add songapprovebtn submitbtn & set up the other files needed for each

---
+++ client/src/components/ForButtons/SubmitBtn/submitbtn.js
@@ -0,0 +1,28 @@
+
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types'
+import axios from "axios";
+
+
+// create stateful component for modal play button
+class SubmitBtn extends Component {
+//  constructor(props) {
+//    super(props);
+
+//    this.state = {
+//      playSong: null
+//    }
+//  }
+  render() {
+    // let {auditionSongLink} = this.props
+
+   return (
+    <Fragment>
+      <button type="button" class="btn btn-primary">Submit</button>
+    </Fragment>
+    )
+  }
+}
+
+  export default TablePlayBtn;
