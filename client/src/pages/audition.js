682e63879a9c7c7e70bf52ad113d1b31b6313fdc
honestgraphics <jeannie@honestgraphics.com>
2018-08-01 00:08:48 -0400

    Create submit button & add to audition.js & manager.js

--- client/src/pages/audition.js
+++ client/src/pages/audition.js
@@ -6,6 +6,7 @@ import api from "../api/api";
 // import components
 import Header from '../components/ForPages/Header/header';
 import AssocTable from '../components/ForTables/1-AssocTable/assoctable';
+import SubmitBtn from '../components/ForButtons/SubmitBtn/submitbtn';
 import Footer from '../components/ForPages/Footer/footer';
 
 
@@ -20,21 +21,35 @@ class AuditionPage extends React.Component {
     api.getAuditions()
     .then((res) => {
       const { data } = res;
+      console.log("this is the tracks for assoctable", this.state.tracks)
+      console.log("this is the res for fetchtrack for assoctable", res)
       this.setState(prevState => ({
         ...prevState,
         tracks: data
       }));
+      
     })
     .catch(err => {
       debugger
     })
   }
+  findTrack = () => {
+    console.log(this.state)
+  }
+  componentDidMount = () => {
+    this.fetchTrack();
+  }
+
   render() {
+    
+    console.log("rendered")
+    console.log(this.state.tracks)
     return (
       <Fragment>
         {/* <h1 style={sectionStyle}>AUDITION PAGE</h1> */}
-        <Header fetchTrack={this.fetchTrack} />
-        <AssocTable fetchTrack={this.fetchTrack} tracks={this.state.tracks} />
+        <Header fetchTrack={this.findTrack} />
+        <AssocTable tracks={this.state.tracks} fetchTrack={this.fetchTrack} />
+        <SubmitBtn />
         <Footer />
     </Fragment>
     );
