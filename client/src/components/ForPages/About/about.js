7b744ca19c84ecef419503e3f362f4a07a53bcda
honestgraphics <jeannie@honestgraphics.com>
2018-08-02 13:33:31 -0400

    replace paragraph for Waleed

--- client/src/components/ForPages/About/about.js
+++ client/src/components/ForPages/About/about.js
@@ -11,9 +11,7 @@ class About extends React.Component {
       <div className="jumbotron jumbotron-fluid">
       <div className="container aboutBox">
         <h2 className="display-4">ABOUT US</h2>
-        <h3>Audition was generted for a radio station that had 
-        a need for automated system
-        that solves the a number of issues the station was facing, like : </h3>
+        <h3>Audition was created for a radio station that had a need for automated system to solves a number of issues the station was facing, like : </h3>
         <ul>
           <li><h3>Security</h3></li>
           <li><h3>Storage</h3></li>
